95b5d0ba8cc6095b4b175ad698c3259247bccf94
ScottHershberger5 <sahershberger5@gmail.com>
2026-02-05 20:14:10 +0000

    trying to fix fixture error

--- lab6/tests/test_parser.py
+++ lab6/tests/test_parser.py
@@ -1,5 +1,5 @@
-import pytest
-from src.parser import parse_product_basic, parse_availability
+
+from parser import parse_product_basic, parse_availability
 
 
 def test_parse_product_basic_extracts_id(valid_product):
@@ -14,3 +14,17 @@ def test_parse_product_basic_returns_only_id_and_name(valid_product):
     parsed_product = parse_product_basic(valid_product)
 
     assert set(parsed_product) == {"id", "name"}
+
+# availability tests
+
+def test_parse_availability_when_in_stock(valid_product):
+    parsed_availabliity = parse_availability(valid_product)
+    assert parsed_availabliity["in_stock"] == True
+
+def test_parse_availability_when_out_of_stock(product_out_of_stock):
+    parsed_availabliity = parse_availability(product_out_of_stock)
+    assert parsed_availabliity["in_stock"] == False
+
+def test_parse_availability_when_field_missing(minimal_product):
+    parsed_availabliity = parse_availability(minimal_product)
+    assert parsed_availabliity["in_stock"] == False
